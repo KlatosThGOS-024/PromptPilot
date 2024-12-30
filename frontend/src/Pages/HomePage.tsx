6548d6633259aa8cd7cb7cc527f2b6bd5e6a980e
KlatosThGOS-024 <priyanshutiwari878@yahoo.com>
2024-12-30 13:37:33 +0530

    MenuBar

--- frontend/src/Pages/HomePage.tsx
+++ frontend/src/Pages/HomePage.tsx
@@ -1,23 +1,27 @@
 import { Input } from "../components/Input";
+import { MenuBar } from "../components/MenuBar";
 import { MessageBox } from "../components/MessageBox";
 
 export const HomePage = () => {
   return (
     <section>
-      <div
-        className=" bg-lightBlue1 h-screen
-       overflow-y-auto flex-grow flex-shrink px-[18px]"
-      >
-        <div className="mx-auto md:w-[744px] sm:min-w-[630px] ">
-          <div>
-            <MessageBox />
-          </div>
-          <div
-            className="fixed bottom-0 md:w-[744px] sm:min-w-[630px] my-[24px]
+      <div className="flex h-screen">
+        <div className="absolute w-[20%] ">
+          <MenuBar />
+        </div>
+
+        <div className="w-[80%] bg-lightBlue1 overflow-y-auto flex-grow flex-shrink px-[18px]">
+          <div className="mx-auto md:w-[744px] sm:min-w-[630px] ">
+            <div>
+              <MessageBox />
+            </div>
+            <div
+              className="fixed bottom-0 md:w-[744px] sm:min-w-[630px] my-[24px]
            md:translate-x-[6px] min-w-[500px] 
          "
-          >
-            <Input />
+            >
+              <Input />
+            </div>
           </div>
         </div>
       </div>
